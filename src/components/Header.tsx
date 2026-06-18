6c613d177e65ce6475afd44417a99e72af225b1e
63amg0010-cpu <63amg0010@gmail.com>
2026-06-18 12:03:16 +0900

    헤더 우측 '나' 프로필 버튼 삭제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-// 상단 헤더: WitchHunt 로고(아이콘 + 글자) + 우측 프로필 동그라미
+// 상단 헤더: WitchHunt 로고(아이콘 + 글자)
 export default function Header() {
   return (
     <header className="header">
@@ -6,7 +6,6 @@ export default function Header() {
         <img src="/witch-icon.png" alt="" className="header__icon" />
         <img src="/witchhunt-word.png" alt="WitchHunt" className="header__word" />
       </div>
-      <div className="header__profile" aria-label="프로필">나</div>
     </header>
   )
 }
